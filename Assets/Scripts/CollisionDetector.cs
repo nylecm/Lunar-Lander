dc180ffd8906dd709988dcc07a332d428422b39e
Michal Nylec <michalnylec@gmail.com>
2022-02-13 23:33:18 +0000

    Game Over When Player Touches Ground

---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionDetector : MonoBehaviour
+{
+    private List<GameObject> Colliders = new List<GameObject>();
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // private void OnTriggerExit2D(Collider2D other)
+    // {
+    //     if (Colliders.Contains(other.gameObject)) Colliders.Remove(other.gameObject);
+    // }
+}

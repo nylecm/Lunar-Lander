6f2ebfccb6e4cd42c5eff8be6c0ddafd6a00b0d3
Michal Nylec <michalnylec@gmail.com>
2022-04-10 23:17:27 +0100

    Minor tweaks

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /**
  * I am implementing a Lunar Lander Game.
@@ -9,77 +10,74 @@ using UnityEngine;
  *
  * - Basic movement of rocket:
  *   - Rotation & Thrust
- * - Collision Detection, and landing hardness threshold.
+ *   - Collision Detection, and landing hardness threshold.
  */
-
 public class Player : MonoBehaviour
 {
     [SerializeField] private int maxRot;
 
-    private Rigidbody2D rb2;
-    private Vector2 velocity;
-    private Vector2 prevPos;
-    private Vector2 curPos;
-    private float curAngle = 0.0f;
-    private int fuelSupply = 10000;
-    private const float rotIncrement = 0.16f;
-    private const float thrustVelocityIncrement = 0.009f;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    private Rigidbody2D _rb2;
+    private Vector2 _velocity;
+    private Vector2 _prevPos;
+    private Vector2 _curPos;
+    private float _curAngle = 0.0f;
+    private int _fuelSupply = 10000;
+    private const float RotIncrement = 0.16f;
+    private const float ThrustVelocityIncrement = 0.012f;
 
     private void Awake()
     {
-        rb2 = GetComponent<Rigidbody2D>();
-        rb2.gravityScale = 0.3f;
-        curPos = rb2.position;
-        prevPos = curPos;
-        velocity = new Vector2();
-        velocity.x += 1.2f;
-        rb2.velocity = velocity;
+        _rb2 = GetComponent<Rigidbody2D>();
+        _rb2.gravityScale = 0.3f;
+        _curPos = _rb2.position;
+        _prevPos = _curPos;
+        _velocity = new Vector2();
+        _velocity.x = 1.2f;
+        _rb2.velocity = _velocity;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        curPos = rb2.position;
-        if (Input.GetKey(KeyCode.W)) AddThrust();
-        if (Input.GetKey(KeyCode.A)) RotateCW();
-        if (Input.GetKey(KeyCode.D)) RotateACW();
+        _curPos = _rb2.position;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) AddThrust();
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) RotateCW();
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) RotateACW();
 
-        prevPos = curPos;
+        _prevPos = _curPos;
         //prevVelocityY = r
     }
 
+    /**
+     * Adds thrust in the direction the rocket is pointing.
+     */
     private void AddThrust()
     {
-        if (fuelSupply == 0)
+        if (_fuelSupply == 0)
         {
+            Debug.Log("No fuel: Unable to Add Thrust!");
             return;
         }
-        
+
         // todo get rid of framerate dependence.
-        bool posAngle = (curAngle > 0);
-        velocity = rb2.velocity;
+        bool posAngle = (_curAngle > 0);
+        _velocity = _rb2.velocity;
 
         if (posAngle)
         {
-            velocity.y += (float) Math.Cos(ConvertToRadians(curAngle)) * thrustVelocityIncrement;
-            velocity.x += (float) -(Math.Sin(ConvertToRadians(curAngle)) * thrustVelocityIncrement);
+            _velocity.y += (float) Math.Cos(ConvertToRadians(_curAngle)) * ThrustVelocityIncrement;
+            _velocity.x += (float) -(Math.Sin(ConvertToRadians(_curAngle)) * ThrustVelocityIncrement);
         }
         else
         {
-            velocity.y += (float) (Math.Cos(ConvertToRadians(-curAngle)) * thrustVelocityIncrement);
-            velocity.x += (float) (Math.Sin(ConvertToRadians(-curAngle)) * thrustVelocityIncrement);
+            _velocity.y += (float) (Math.Cos(ConvertToRadians(-_curAngle)) * ThrustVelocityIncrement);
+            _velocity.x += (float) (Math.Sin(ConvertToRadians(-_curAngle)) * ThrustVelocityIncrement);
         }
 
         //velocity.y += 0.01f;
-        rb2.velocity = velocity;
-        fuelSupply -= 1;
-        Debug.Log(fuelSupply);
+        _rb2.velocity = _velocity;
+        _fuelSupply -= 1;
+        Debug.Log(_fuelSupply);
         //rb2.velocity.x = velocityX;
     }
 
@@ -87,50 +85,48 @@ public class Player : MonoBehaviour
     {
         return (Math.PI / 180) * angle;
     }
-    
+
     private void RotateCW()
     {
-        if (curAngle < -90)
+        if (_curAngle < -90)
         {
             return;
         }
 
-        rb2.transform.Rotate(0, 0, -rotIncrement, Space.Self);
-        curAngle -= rotIncrement;
-        Debug.Log(curAngle);
+        _rb2.transform.Rotate(0, 0, -RotIncrement, Space.Self);
+        _curAngle -= RotIncrement;
+        Debug.Log(_curAngle);
     }
 
     private void RotateACW()
     {
-        if (curAngle > 90)
+        if (_curAngle > 90)
         {
             return;
         }
 
-        rb2.transform.Rotate(0, 0, rotIncrement, Space.Self);
-        curAngle += rotIncrement;
-        Debug.Log(curAngle);
+        _rb2.transform.Rotate(0, 0, RotIncrement, Space.Self);
+        _curAngle += RotIncrement;
+        Debug.Log(_curAngle);
     }
 
     [UsedImplicitly]
     public void Landed()
     {
-        if (rb2.velocity.y < -0.5f)
+        if (_rb2.velocity.y < -1.4f)
         {
-            Debug.Log(rb2.velocity.y + "Bang!!!");
-            //SceneManager.LoadScene("SampleScene"); todo uncomment when done
+            Debug.Log(_rb2.velocity.y + "Bang!!!");
+            SceneManager.LoadScene("SampleScene");
         }
-        else if (rb2.velocity.y < -0.25f)
+        else if (_rb2.velocity.y < -0.7f)
         {
-            
-            Debug.Log(rb2.velocity.y + "Hard Landing!");
-            //SceneManager.LoadScene("SampleScene"); todo uncomment when done
-
+            Debug.Log(_rb2.velocity.y + "Hard Landing!");
+            SceneManager.LoadScene("SampleScene");
         }
         else
         {
-            Debug.Log(rb2.velocity.y + "BUTTER :)");
-            //SceneManager.LoadScene("SampleScene"); todo uncomment when done
+            Debug.Log(_rb2.velocity.y + "BUTTER :)");
+            SceneManager.LoadScene("SampleScene");
         }
     }
 }

4661f23e8c2b4ae100848f3aa210e2fab74b3f27
Michal Nylec <michalnylec@gmail.com>
2022-04-23 02:59:39 +0100

    All lander selector play buttons are working, images of lander sprites added.

---
+++ Assets/Scripts/PowerUp/ExtraFuelPowerUp.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ExtraFuelPowerUp : PowerUp
+{
+    public ExtraFuelPowerUp(AudioClip acquiredSound) : base(acquiredSound)
+    {
+    }
+
+    public override void Activate()
+    {
+        throw new System.NotImplementedException();
+    }
+}

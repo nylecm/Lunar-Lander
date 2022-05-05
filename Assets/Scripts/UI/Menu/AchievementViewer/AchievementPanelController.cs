7d4bd3b4b545b1ff96e65030c5fcc7dd552220c7
Michal Nylec <michalnylec@gmail.com>
2022-05-05 05:31:13 +0100

    achievement ui feature complete.

--- Assets/Scripts/UI/Menu/AchievementViewer/AchievementPanelController.cs
+++ Assets/Scripts/UI/Menu/AchievementViewer/AchievementPanelController.cs
@@ -9,7 +9,20 @@ public class AchievementPanelController : MonoBehaviour
     private void OnEnable()
     {
         var components = GetComponentsInChildren<TextMeshProUGUI>();
-        components[0].text = ProfileManager.CurProfile.GetAchievementOfType(achievementType).Name;
-        components[1].text = ProfileManager.CurProfile.GetAchievementOfType(achievementType).CurProgress.ToString(); // 2=> rotation
+        AchievementModel thisAchievement = ProfileManager.CurProfile.GetAchievementOfType(achievementType);
+        if (ProfileManager.CurProfile.GetAchievementOfType(achievementType) == null)
+        {
+            components[0].text = AchievementFactory.MakeAchievementOfType(achievementType).Name;
+            components[1].text = "Locked";
+        }
+        else
+        {
+            components[0].text = thisAchievement.Name;
+            if (thisAchievement.IsComplete())
+                components[1].text = "Unlocked";
+            else
+                components[1].text = "Progress: " + thisAchievement.CurProgress + "/" + thisAchievement.ProgressRequired;
+        }
+
     }
 }
